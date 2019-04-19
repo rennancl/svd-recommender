b4cb91e86df26db55dcd219a6bbbbe185e5bdc8d
Rennan Cordeiro <rennancl@gmail.com>
2019-04-19 16:19:17 -0300

    io functions

--- main.cpp
+++ main.cpp
@@ -18,17 +18,33 @@ pair<int,int> max_user_item(vector<tuple<int, int, int, int>> vetor){
             max_user = get<0>(vetor[i]);
         }    
     }
+
     for(unsigned i = 0; i < vetor.size(); i++){
         if(get<1>(vetor[i]) > max_item){
-            max_item = get<0>(vetor[i]);
-            cout << max_item  << endl;
+            max_item = get<1>(vetor[i]);
         }    
     }
+
     max_values.first = max_user;
     max_values.second = max_item;
     return max_values;
 }
     
+int** create_matrix(vector<tuple<int, int, int, int>> input_vector  vector<int>(make_tuple(0,0,0,0)), pair<int,int> dimentions, bool fill = false){
+
+    int** matrix = new int*[dimentions.first + 1];
+    for (int i = 0; i < dimentions.first + 1; i++)
+    {
+        matrix[i] = new int[dimentions.second + 1];
+    }
+    
+    if(fill){
+        for(unsigned i = 0; i < input_vector.size(); i++){
+            matrix[get<0>(input_vector[i])][get<1>(input_vector[i])] = get<2>(input_vector[i]);
+        }
+    }
+    return matrix;
+}
 vector<tuple<int, int, int, int>> process_inputs(string filename){
     ifstream file;
     file.open(filename);
@@ -77,36 +93,56 @@ vector<tuple<int, int, int, int>> process_inputs(string filename){
     return inputs;
 }
 
-int main(int argc, char *argv[])
-{    
+void create_predictions_file(int predictions){
+    cout << predictions << endl;
+    return;
+}
+
+pair<int**, int**> stochastic_gradient_descent(int **matrix,pair<int,int> dimentions, int k){
+    pair<int**, int**> matrixes;
     
+    pair<int, int> p_dimentions;
+    pair<int, int> q_dimentions;
+
+    p_dimentions.first = dimentions.first;
+    p_dimentions.second = k;
+
+    q_dimentions.first = k;
+    q_dimentions.second = dimentions.second;
+
+    int **p_matrix  = create_matrix(matrix, p_dimentions);
+    int **q_matrix  = create_matrix(matrix, q_dimentions);
+    
+    bool converged = false;
+    while(1){    
+        converged = true;
+        if(converged) break;
+    }
+
+    matrixes.first = p_matrix;
+    matrixes.second = q_matrix;
+    return matrixes;
+}
+
+int main(int argc, char *argv[])
+{ 
     string ratings_filename = argv[1];
     string targets_filename = argv[2];
 
     vector<tuple<int, int, int, int>> ratings= process_inputs(ratings_filename);
     vector<tuple<int, int, int, int>> targets= process_inputs(targets_filename);
 
-    pair<int,int> max_values = max_user_item(ratings);
+    pair<int,int> dimentions = max_user_item(ratings);
+    int** matrix = create_matrix(ratings, dimentions, true);
 
-    int** user_item_matrix = new int*[max_values.first + 1];
-    for (int i = 0; i < max_values.first + 1; i++)
-    {
-        user_item_matrix[i] = new int[max_values.second];
-        for (int j = 0; j < max_values.first; j++)
-        {
-            user_item_matrix[i][j] = 0;
-        }
-    }
-    for(unsigned i = 0; i < ratings.size(); i++){
-        // user_item_matrix[get<0>(ratings[i])][get<1>(ratings[i])] = get<2>(ratings[i]);
-    }
+    int** p_matrix;
+    int** q_matrix;
+    pair<int**, int**> pqmatrix;
 
+    pqmatrix = stochastic_gradient_descent(matrix,dimentions, 30);
+    p_matrix = pqmatrix.first;
+    q_matrix = pqmatrix.second;
+    
+    create_predictions_file(1);
     return 0;
 }
-// for(unsigned i = 0; i < ratings.size(); i++){
-//     cout << get<0>(ratings[i]) << "-" << get<1>(ratings[i]) <<  "-"  << get<2>(ratings[i]) <<  "-"  << get<3>(ratings[i]) << endl;    
-// }
-
-// for(unsigned i = 0; i < targets.size(); i++){
-//     cout << get<0>(targets[i]) << "-" << get<1>(targets[i]) << endl;    
-// }
